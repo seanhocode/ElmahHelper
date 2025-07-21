5c93ba6af1305a432fb60624bc13990bf3664daf
sean_ho <sean_ho@gss.com.tw>
2025-07-21 10:50:31 +0800

    20250721 New Create

---
+++ ElmahHelper/Model/Error.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace ElmahHelper.Model
+{
+    public class Error
+    {
+        public DateTime Time { get; set; }
+
+        private string ErrorID { get; set; }
+
+        private string Application { get; set; }
+
+        private string Host { get; set; }
+
+        private string Type { get; set; }
+
+        public string Message { get; set; }
+
+        private string Source { get; set; }
+
+        private string Detail { get; set; }
+
+        public void SetInfo(XDocument info)
+        {
+            var errorElement = info.Descendants("error").FirstOrDefault();
+            if (errorElement == null) return;
+
+            ErrorID = errorElement.Attribute("errorId")?.Value;
+            Application = errorElement.Attribute("application")?.Value;
+            Host = errorElement.Attribute("host")?.Value;
+            Type = errorElement.Attribute("type")?.Value;
+            Message = errorElement.Attribute("message")?.Value;
+            Source = errorElement.Attribute("source")?.Value;
+            Detail = errorElement.Attribute("detail")?.Value;
+            string timeAttr = errorElement.Attribute("time")?.Value;
+            if (DateTime.TryParse(timeAttr, out DateTime time))
+            {
+                Time = time;
+            }
+        }
+
+        public void SetEmptyInfo(string elmahName)
+        {
+            Message = "讀取失敗";
+            Detail = $"{elmahName}讀取失敗";
+        }
+
+        public string GetDetail()
+        {
+            return Detail;
+        }
+    }
+}
---
+++ ElmahHelper/Service/ElmahService.cs
@@ -0,0 +1,48 @@
+﻿using ElmahHelper.Model;
+using ElmahHelper.Tools;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ElmahHelper.Service
+{
+    public class ElmahService
+    {
+        private ZipTool zipTool = new ZipTool();
+        private FileTool fileTool = new FileTool();
+
+        /// <summary>
+        /// 取得Elmah清單
+        /// </summary>
+        /// <param name="elmahFolderPath"></param>
+        /// <returns></returns>
+        public IList<Elmah> GetElmahList(string elmahFolderPath, DateTime startTime, DateTime endTime)
+        {
+            IList<Elmah> elmahList = new List<Elmah>();
+
+            IList<string> filePathList = fileTool.GetAllFileInFolder(elmahFolderPath);
+
+            DateTime elmahTime;
+
+            if(filePathList != null)
+                foreach (string filePath in filePathList)
+                {
+                    if(filePath.EndsWith(".zip")){
+                        foreach (string fileName in zipTool.GetFileNameInZip(filePath)){
+                            elmahTime = Elmah.GetElmahFileNameData(fileName).Value.ElmahTime;
+                            if ((elmahTime >= startTime) && (elmahTime <= endTime))
+                                elmahList.Add(new Elmah(fileName, filePath));
+                        }
+                    }
+                    else{
+                        elmahTime = Elmah.GetElmahFileNameData(Path.GetFileName(filePath)).Value.ElmahTime;
+                        if ((elmahTime >= startTime) && (elmahTime <= endTime))
+                            elmahList.Add(new Elmah(filePath));
+                    }
+                }
+
+            return elmahList;
+        }
+    }
+
+}
